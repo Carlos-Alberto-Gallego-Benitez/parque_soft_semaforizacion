cb15bea4794d97df4b1a250c18dd0097b7c36a94
Armando <gomezuruetaarmandojose@gmail.com>
2023-02-08 17:26:12 -0500

    actualizacion

--- administrativa/src/services/Service.js
+++ administrativa/src/services/Service.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const basePath = process.env.VUE_APP_API_URL;
+//const basePath = process.env.VUE_APP_API_URL;
 
+const basePath = 'http://127.0.0.1:8000';
 export default axios.create({
     baseURL: basePath,
     withCredentials: true
