8f65b9fd03173da447a21cba7c28c79454713dc3
Carlos Benítez <carlosalgabez.08@gmail.com>
2023-04-16 10:57:52 -0500

    ajustes

--- empresas/src/components/Footer.vue
+++ empresas/src/components/Footer.vue
@@ -5,7 +5,7 @@
   <footer id="footer">
     <div class="container">
       <div class="copyright">
-        &copy; Copyright <strong><span><img src="../assets/img/logo.png" alt="" width="200"></span></strong>
+        &copy; Copyright <strong><span><img src="../assets/img/logoNew.jpeg" alt="" width="200"></span></strong>
       </div>
       <div class="credits">
         Desarrollado por Parquesoft
